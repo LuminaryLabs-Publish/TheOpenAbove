9de60590a3a31285a3d5b0e930eebfb3cb087596
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-16 14:20:21 -0400

    refactor(domains): export image capture domain

--- src/domains/index.js
+++ src/domains/index.js
@@ -3,5 +3,5 @@ export * from "./ballooning/ballooning-domain.js";
 export * from "./sky/sky-domain.js";
 export * from "./land/land-domain.js";
 export * from "./navigation/navigation-domain.js";
-export * from "./air-mail/air-mail-domain.js";
+export * from "./image-capture/image-capture-domain.js";
 export * from "./experience/experience-domain.js";
