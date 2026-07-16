14ee5cbc4bb11c1a288efeb58cb0967f9dfb1b2d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-16 14:05:17 -0400

    refactor(domains): export semantic domains

---
+++ src/domains/index.js
@@ -0,0 +1,7 @@
+export * from "./journey/journey-domain.js";
+export * from "./ballooning/ballooning-domain.js";
+export * from "./sky/sky-domain.js";
+export * from "./land/land-domain.js";
+export * from "./navigation/navigation-domain.js";
+export * from "./air-mail/air-mail-domain.js";
+export * from "./experience/experience-domain.js";
