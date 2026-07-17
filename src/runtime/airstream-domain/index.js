f005771a4fdf0a23b2d6360982ed1df96187572a
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-17 15:19:23 -0400

    refactor(wind): stop exporting legacy route visuals

--- src/runtime/airstream-domain/index.js
+++ src/runtime/airstream-domain/index.js
@@ -2,6 +2,5 @@ export * from "./airstream-route-kit.js";
 export * from "./airstream-sampler-kit.js";
 export * from "./airstream-field-kit.js";
 export * from "./airstream-balloon-force-kit.js";
-export * from "./airstream-visual-kit.js";
 export * from "./airstream-debug-kit.js";
 export * from "./airstream-domain.js";
